cb61cf7c35de24d0467eead739d9f6c8989855c9
arthurjohnson17 <arthurjohnson17@gmail.com>
2026-02-10 17:31:53 +0000

    Update constants.ts

--- lib/constants.ts
+++ lib/constants.ts
@@ -50,6 +50,6 @@ const ALLOWED_TYPES = {
 
 const MAX_FILE_SIZE = 100 * 1024 * 1024;
 
-const UPLOAD_DIR = "/tmp/uploads";
+const UPLOAD_DIR = "changes";
 
 export { ALLOWED_TYPES, MAX_FILE_SIZE, UPLOAD_DIR };
