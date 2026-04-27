16e4365b4487ccdff7565ee6941d78942708d84a
arthurjohnson17 <arthurjohnson17@gmail.com>
2026-04-27 12:04:52 +0100

    Update route.ts

--- app/api/[...all]/route.ts
+++ app/api/[...all]/route.ts
@@ -1,4 +1,8 @@
 import { auth } from '@/lib/better-auth/auth'
 import { toNextJsHandler } from 'better-auth/next-js'
 
-export const { GET, POST } = toNextJsHandler(auth.handler)
+if (!auth) {
+    throw new Error("Auth instance is not initialized");
+}
+
+export const { GET, POST } = toNextJsHandler(auth.handler);
